72cbf1742aa6dae284798ad469d2375e1d1f7f59
Dinchatbot <malthekchristensen@gmail.COM>
2026-01-18 20:55:48 +0100

    Use process.env.PORT for Render

--- server.js
+++ server.js
@@ -25,6 +25,8 @@ app.post("/chat", (req, res) => {
   }
 });
 
-app.listen(3000, () => {
-  console.log("Server kører på http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server kører på port ${PORT}`);
 });
